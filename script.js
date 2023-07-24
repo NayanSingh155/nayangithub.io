8dc69b6a7c3f5ca2ba85af8c9b2f6d3fc7c89e23
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2023-07-24 11:38:31 +0530

    all done

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Full Stack Developer", "Designer", "MERN Stack Developer"],
+    strings: ["Full Stack Developer 💻", "Designer 🖋 ", "MERN Stack Devloper 💻","Enthusiastic Dev 😎"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Mern Stack Developer", "Designer"],
+    strings: ["Full Stack Developer 💻", "Designer 🖋", "MERN Stack Devloper 💻","Enthusiastic Dev 😎"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
